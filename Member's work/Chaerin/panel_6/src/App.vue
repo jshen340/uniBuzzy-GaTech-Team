c2d7ce6a0b2ef722d27b98fed57bf0f2c9538250
chaerinlee5 <133067951+chaerinlee5@users.noreply.github.com>
2023-10-26 23:26:35 -0400

    panel 6 progress

---
+++ Member's work/Chaerin/panel_6/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Panel_6 msg="Panel 6.0"/>
+</template>
+
+<script>
+import Panel_6 from './components/Panel_6.0.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Panel_6
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Member's work/Chaerin/panel_6/src/components/Panel_6.0.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="match" style="height: 200px;">
+        <h2>You matched with {{ name }} !</h2> <!-- add name variable from their name -->
+    </div>
+    <div class="box">
+        <ul id="list">
+            <li>
+                <img class="emoji8" v-bind:src="'../assets/unib_emoji_8'"/>
+            </li>
+            <li>
+                <h4>Location{{Location}}</h4> <!-- add location variable from matched location -->
+            </li>
+            <li>
+                <h4>Clothing{{Clothing}}</h4> <!-- add clothing variable from clothing -->
+            </li>
+        </ul>
+    </div>
+    
+</template>
+
+<style>
+    .box {
+        margin: 0 auto;
+        width: 50%;
+    }
+    #list {
+        width: 50%;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .emoji8{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+</style>
+
+<script>
+    export default {
+  name: "Panel_6",
+}
+</script>
